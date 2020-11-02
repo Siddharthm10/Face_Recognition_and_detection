136d23d251f849f44d814504a9a10b3a561a30e3
siddharthm10 <mehtasid130499@gmail.com>
2020-11-02 14:36:38 +0530

    comments added to motion detection

--- motion_detection/m_detector.py
+++ motion_detection/m_detector.py
@@ -1,3 +1,4 @@
+#Importing the libraries
 import cv2
 import numpy as np
 import time 
@@ -6,10 +7,10 @@ from datetime import datetime
 
 
 #initializing variables
-first_frame = None
-status_list = [None,None]
-times = []
-df = pd.DataFrame(columns = ["Start", "End"])
+first_frame = None #Stagnant frame from which we'll compare
+status_list = [None,None] # This keeps a record of motion
+times = [] #This keeps the time stamp of detected motion (different for different objects)
+df = pd.DataFrame(columns = ["Start", "End"]) #columns to keep the record of motionstart and motionend
 
 # cv2.namedWindow("output", cv2.WINDOW_NORMAL) 
 # cv2.resizeWindow("output", 1920, 1080)
@@ -25,50 +26,63 @@ while(True):
     #Grayscale Conversion
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray,(21,21),0)
+    # So that the first frame is read only once
     if first_frame is None:
         first_frame = gray
         continue
 
-    #Calculates difference
+    #Calculates difference to detect motion
     delta_frame = cv2.absdiff(first_frame, gray)
     #Applies Threshold
     thresh_delta = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
     thresh_delta = cv2.dilate(thresh_delta, None, iterations=0)
-    #finding contours on the white portion
+    #finding contours on the white portion(made by the threshold)
     cnts,_ = cv2.findContours(thresh_delta.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    #ignoring the Small changes
+    
     for contour in cnts:
+        #Ignoring small Motions, i.e. noise and small insects
         if cv2.contourArea(contour)<1000:
             continue
+        #motion Detected
         status = 1
         #Drawing the rectangles around the contours identified
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+        #saving the image of the moving object
         cv2.imwrite("motion_detection/spotted.jpg", frame)
+    
+    #appending the motion detection status
     status_list.append(status)
 
+    # Trimming the the status list to last two objects
     status_list = status_list[-2:]
+    #It means that the object has entered the frame
     if status_list[-1]==1 and status_list[-2]==0:
         times.append(datetime.now())
+    #It means that the object moving has left the frame
     if status_list[-1]==0 and status_list[-2]==1:
         times.append(datetime.now())
     
     # Show Output
-    cv2.imshow("frame",frame)
-    cv2.imshow('capturing', gray)
-    cv2.imshow('delta', delta_frame)
-    cv2.imshow('thresh', thresh_delta)
+    cv2.imshow('capturing', gray)#gray scale image
+    cv2.imshow('delta', delta_frame)#difference btween the first and current frame
+    cv2.imshow('thresh', thresh_delta)#thresholded frame
+    cv2.imshow("frame",frame)#Frame
 
-    #Quiting
+    #Quiting (Reading the key)
     key = cv2.waitKey(1) 
     if key == ord('q'):
         break
+
 print(status_list)
 print(times)
+#Appending the details (time) in the dataframe
 for i in range(0, len(times),2):
     df = df.append({"Start": times[i], "End":times[i+1]},ignore_index=True)
+#Writing the csv file with the entry and exit time of the objects
 df.to_csv("motion_detection/Times.csv")
 
+#Closing the camera access and the windows.
 cap.release()
 cv2.destroyAllWindows()
