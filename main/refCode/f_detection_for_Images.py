4f8a66ac096ac728da07606164a1da4f6b97fedb
siddharthm10 <mehtasid130499@gmail.com>
2020-11-11 11:25:53 +0530

    WorkFlow Update

--- main/refCode/f_detection_for_Images.py
+++ main/refCode/f_detection_for_Images.py
@@ -40,45 +40,45 @@ if __name__ == "__main__":
     encodeListKnown = list(data.values())
 
     # cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture('rtsp://admin:admin123@192.168.0.104:554/')
+    # cap = cv2.VideoCapture('rtsp://admin:admin123@192.168.0.104:554/')
 
     # count = 0
-    while True:
-        
-        success, img = cap.read()
-        img = img[:][150:]
-        imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
-        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-        
 
-        facesCurFrame = fr.face_locations(imgS)
-        encodeCurFrame = fr.face_encodings(imgS, facesCurFrame)
-        for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
-            matches = fr.compare_faces(encodeListKnown, encodeFace)
-            faceDis = fr.face_distance(encodeListKnown, encodeFace)
+    
+    img = fr.load_image_file("main/sid.jpeg")
+    # img = img[:][150:]
+    imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
+    # imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    
 
-            matchIndex = np.argmin(faceDis)
-            if faceDis[matchIndex]<tolerance:
-                if matches[matchIndex]:
-                    name = classNames[matchIndex]
-                    y1, x2, y2, x1 = faceLoc
-                    y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
-                    cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0),2)
-                    cv2.rectangle(img, (x1,y2-35), (x2,y2),(0,255,255), cv2.FILLED)
-                    cv2.putText(img, name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0),2)
-                    cv2.imwrite("output.jpg", img)    
-                    markAttendance(name)
-            else:
+    facesCurFrame = fr.face_locations(imgS)
+    encodeCurFrame = fr.face_encodings(imgS, facesCurFrame)
+    for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
+        matches = fr.compare_faces(encodeListKnown, encodeFace)
+        faceDis = fr.face_distance(encodeListKnown, encodeFace)
+
+        matchIndex = np.argmin(faceDis)
+        if faceDis[matchIndex]<tolerance:
+            if matches[matchIndex]:
+                name = classNames[matchIndex]
                 y1, x2, y2, x1 = faceLoc
                 y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
                 cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0),2)
                 cv2.rectangle(img, (x1,y2-35), (x2,y2),(0,255,255), cv2.FILLED)
                 cv2.putText(img, name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0),2)
+                cv2.imwrite("output.jpg", img)    
+                markAttendance(name)
+        else:
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+            cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0),2)
+            cv2.rectangle(img, (x1,y2-35), (x2,y2),(0,255,255), cv2.FILLED)
+            cv2.putText(img, name, (x1+6,y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,0),2)
 
- 
-        cv2.imshow("Webcam", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    cv2.imshow("Webcam", img)
+    print(facesCurFrame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
         
         # if(count>120):
         #     break
@@ -88,6 +88,5 @@ if __name__ == "__main__":
     # print(fps) #- 5.2677 at 1/4 times resolution
                # - 9.026 at 1/2 times resolution
 
-    cap.release()
-    cv2.destroyAllWindows()
+    
 
