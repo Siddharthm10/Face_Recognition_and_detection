1fd5a2bb7303d536e976add3afe4ff59503086d3
Siddharthm10 <siddharth.mehtaid@gmail.com>
2020-11-20 10:22:19 +0530

    Git ignore Added

--- camera_access/access_CCTV.py
+++ camera_access/access_CCTV.py
@@ -6,12 +6,12 @@ import os
 #resized to 534*400(after trimming the time stamp)
 # cv2.namedWindow("output", cv2.WINDOW_NORMAL) 
 # cv2.resizeWindow("output", 1920, 1080)
-cap = cv2.VideoCapture('rtsp://admin:admin123@192.168.0.104/')
+cap = cv2.VideoCapture('rtsp://admin:admin123@192.168.0.104:554/')
 
 # print("after")
 while(True):
     ret, frame = cap.read()
-    frame = frame[:][100:]
+    # frame = frame[:][100:]
     frame = cv2.resize(frame, (534,400))
     cv2.imshow("output",frame)
     print(frame.shape)
